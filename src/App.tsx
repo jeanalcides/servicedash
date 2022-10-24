62ff61875e8f4b4e79adc4cba018bedb2019fa4f
Jean Alcides de Jesus <jean.jesus@marche.com.br>
2022-10-24 08:48:35 -0300

    Creation and first configurations of project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+function App() {
+
+  return (
+    <div className="w-[300px] rounded p-5 bg-gray-900">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
